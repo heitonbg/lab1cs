80f3a781c85768ce942a17d6acba9178f59e2267
iwrawww@gmail.com <iwrawww@gmail.com>
2025-02-22 23:10:57 +0700

    степени

--- Program.cs
+++ Program.cs
@@ -8,4 +8,20 @@ using System.Threading.Tasks;
 
 namespace laba1
 {
+    class Program
+    {
+        static void Degree()
+        {
+            Console.WriteLine("Введите число, которое хотите возвести в степень: ");
+            int result = 1;
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень, в которую хотите возвести число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int degree = 1; degree < n + 1; ++degree)
+            {
+                result *= a;
+            }
+            Console.WriteLine($"Ваш ответ {result}");
+        }
+    }
 }
